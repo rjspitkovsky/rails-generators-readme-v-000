4688185cd739d9dc8e81c2571780c51758d00d9d
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-08 19:16:25 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,14 @@ Rails.application.routes.draw do
 
   get 'admin/settings'
 
+  get 'admin/dashboard'
+
+  get 'admin/stats'
+
+  get 'admin/financials'
+
+  get 'admin/settings'
+
   get 'categories/show'
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
